5a5de8313913b7d641a6dbf62106436dee92ed79
LindeAkira <ajlindeman232@gmail.com>
2024-05-20 12:23:00 +1200

    Make individual instrument pages

--- InstrumentWebsite/routes.py
+++ InstrumentWebsite/routes.py
@@ -1,38 +1,47 @@
 from flask import Flask, render_template
 import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
 
-@app.route('/home')
+@app.route('/')
 def homepage():
     return render_template("home.html")
 
 
-@app.route('/signup')
+@app.route('/signup, methods=["GET", "POST"]')
 def signup():
-    return render_template("signup.html")
+    # If the user made a POST request, create a new user
+    if request.method == "POST":
+        user = Users(username=request.form.get("username"),
+                     password=request.form.get("password"))
+        # Add the user to the database
+        db.session.add(user)
+        # Commit the changes made
+        db.session.commit()
+        # Once user account created, redirect them
+        # to login route (created later on)
+        return redirect(url_for("login"))
+    # Renders sign_up template if user made a GET request
+    return render_template("sign_up.html")
 
 @app.route('/login')
 def login():
     return render_template("login.html")
 
-@app.route('/comments')
-def comments():
-    instr = 
-    return render_template("comments.html")
-
 # Individual instrument details page.
-@app.route('/intstrument/<int:id>')
-def teddy_details(id):
-  # print("The instrument id is {}".format(id))  # DEBUG
-  conn = sqlite3.connect(app.config['DATABASE'])
-  cur = conn.cursor()
-  cur.execute("SELECT * FROM Instrument WHERE id=?;",(id,))
-  # fetchone returns a tuple containing the data for one entry
-  teddy = cur.fetchone()
-  conn.close()
-  return render_template("instrument.html", teddy=teddy)
+@app.route('/instrument/<int:id>')
+def instrument_details(id):
+    # print("The instrument id is {}".format(id))  # DEBUG
+    conn = sqlite3.connect("instruments.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Instrument WHERE id=?;", (id,))
+    # fetchone returns a tuple containing the data for one entry
+    instrument = cur.fetchone()
+    conn.close()
+    return render_template("instrument.html", instrument=instrument)
 
 @app.route('/string')
 def string():
@@ -40,7 +49,8 @@ def string():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Instrument WHERE familyid = 1")
     string = cursor.fetchall()
-    return render_template("string.html", string=string)
+    print(string)
+    return render_template("string.html", results=string)
 
 @app.route('/woodwind')
 def woodwind():
@@ -48,7 +58,7 @@ def woodwind():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Instrument WHERE familyid = 2")
     woodwind = cursor.fetchall()
-    return render_template("woodwind.html", woodwind=woodwind)
+    return render_template("woodwind.html", results=woodwind)
 
 @app.route('/brass')
 def brass():
@@ -56,7 +66,7 @@ def brass():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Instrument WHERE familyid = 3")
     brass = cursor.fetchall()
-    return render_template("brass.html", brass=brass)
+    return render_template("brass.html", results=brass)
 
 @app.route('/percussion')
 def percussion():
@@ -64,7 +74,7 @@ def percussion():
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM Instrument WHERE familyid = 4")
     percussion = cursor.fetchall()
-    return render_template("percussion.html", percussion=percussion)
+    return render_template("percussion.html", results=percussion)
 
 # must be the last two lines of code for website
 if __name__ == '__main__':
